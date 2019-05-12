f5cda217fc58e84cfab5db5f4b03def2a59b826d
Alex Shor <nhsome@ya.ru>
2019-05-12 15:18:05 +0300

    Added 14.sh

--- L1c/14.sh
+++ L1c/14.sh
@@ -13,3 +13,26 @@
 # применении скрипта для данного процесса. Параметр: в
 # общем случае полная строка команды, породившей
 # процесс.
+
+# Запуск: ./14.sh /opt/google/chrome/chrome
+
+outputDir="11output/"
+command=$1
+
+commandHash=`echo -n $command | shasum`
+commandHash="${commandHash%  -*}"
+
+filePath=$outputDir$commandHash
+data=`ps aux | grep $command | head -1`
+data=($data)
+data=`date`' '${data[9]}' '${data[10]}
+
+if [ -f "$filePath" ]
+then
+	echo "Not first execution. Log file: $filePath"
+else
+	echo "First execution. Log file: $filePath"
+	echo "Timestamp                   CPUTIME COMMAND" > $filePath
+fi
+
+echo $data >> $filePath
