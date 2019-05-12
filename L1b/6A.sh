7e1965949f45bc6d46bbbfb98aa066b6489e9171
pereland <34776726+pereland@users.noreply.github.com>
2019-05-12 22:00:16 +0300

    Update 6A.sh

--- L1b/6A.sh
+++ L1b/6A.sh
@@ -18,7 +18,7 @@
 archiveName=$1
 shift
 
-tempDir="${archiveName%%.*}"
+tempDir="${archiveName%%.*}"  # название tempDir = название архива без расширения(й)
 
 mkdir $tempDir
 
@@ -31,14 +31,14 @@ archiveExtn=${archiveName#*.}
 
 case "$archiveExtn" in
 	zip)
-		zip -r $archiveName $tempDir
+		zip -r $archiveName $tempDir # -r - архивация папки
 		;;
 	tar.bz2)
-		tar cjf $archiveName $tempDir
+		tar cjf $archiveName $tempDir # c - создать новый архив, j - для bz2, f - обозначить имя архива
 		;;
 	*)
 		echo "Архивный формат не распознан."
 		;;
 esac
 
-rm -rf $tempDir
+rm -rf $tempDir # -rf - удаляем каталог (-r) без запроса подтверждения (-f)
